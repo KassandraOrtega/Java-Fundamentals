8f3a55ddf4674327190fb6b18d68d5642079fb1b
KassandraOrtega <78051731+KassandraOrtega@users.noreply.github.com>
2021-01-29 11:52:08 -0600

    Add files via upload

---
+++ src/com/acme/utils/MyDate.java
@@ -0,0 +1,84 @@
+package com.acme.utils;
+
+public class MyDate {
+	private byte month;
+	private byte day;
+	private short year;
+	
+	public MyDate(){
+		this(1,1,1900);
+	}
+	
+	public MyDate(int m, int d, int y){
+		setDate(m, d, y);
+	}
+	
+	public void setDate(int m, int d, int y) {
+		if (valid(d, m, y))
+		{
+			month = (byte)m;
+			day = (byte)d;
+			year = (short)y;
+		}
+	}
+	
+	public String toString() {
+		return "The date is: " + month + "/" + day + "/" + year;
+	}
+	
+	public static void leapYears() {
+		for (int i = 1752; i <= 2020; i = i + 4) {
+			if ((i % 100 != 0) || (i % 400 == 0))
+				System.out.println("The year " + i + " is a leap year"); 
+		}
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		if (valid(day, month, year))
+		{
+			this.month = (byte)month;
+		}
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		if (valid(day, month, year))
+		{
+			this.day = (byte)day;
+		}
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		if (valid(day, month, year))
+		{
+			this.year = (short)year;
+		}
+	}
+	
+	private boolean valid(int day, int month,int year) {
+		if (day > 31 || day < 1 || month > 12 || month < 1) {
+			System.out.println("Attempting to create a non-valid date " 
+					+ month + "/" + day + "/" + year);
+			return false;
+		}
+		switch(month) {
+			case 4:
+			case 6:
+			case 9:
+			case 11: return (day <= 30);
+			case 2: return day <= 28 || (day == 29 && year % 4 == 0);
+		}
+		return true;
+	}
+}
